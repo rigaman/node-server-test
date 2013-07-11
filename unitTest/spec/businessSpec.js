37198d238a0163f2dcef25e77c6d981a86c5c383
Philip <philip@highground.com>
2013-07-11 09:30:17 -0500

    adding pattern using event emitters

--- unitTest/spec/businessSpec.js
+++ unitTest/spec/businessSpec.js
@@ -1,4 +1,8 @@
-var business = require('../../lib/business/test1.js');
+var business = require('../../lib/business/test1.js'),
+	business2 = require('../../lib/business/test2.js'),
+	events = require('events'),
+	eventEmitter = new events.EventEmitter();
+
 describe("Business unit tests", function(){
 	it("Expect business module to retrun proper json", function(){
 		business.getJson({clientId: 1, testId: 1}, function(err, data){
@@ -6,5 +10,20 @@ describe("Business unit tests", function(){
 			expect(data.testId).toBe(1);
 			expect(data.count).toBe(1);
 		});
+	});
+	it("Expect business module to retrun proper json", function(){
+		// var result;
+		// business2.getJson({clientId: 1, testId: 1});
+		// eventEmitter.on('getJsonComplete', function(data){
+		// 	console.log(data);
+		// 	result = data;
+		// });
+		// waitsFor(function(){ return result;});
+		// runs(function(){
+  // 			expect(data.clientId).toBe(1);
+		// 	expect(data.testId).toBe(1);
+		// 	expect(data.count).toBe(1);
+		// });
+
 	});
 });
---
+++ unitTest1/spec/serverSpec2.js
@@ -0,0 +1,386 @@
+var caller = require('../../lib/caller.js');
+describe("Server spec 4", function(){
+	var Caller;
+    beforeEach(function(){
+       //Caller = new caller;
+    });
+
+	it("Test 1 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+		var ret;
+		runs(function() {
+			caller.getService2(1, 1, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             expect(ret.testId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 2 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+		var ret;
+		runs(function() {
+			caller.getService2(1, 2, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('2');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 3 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 3, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('3');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 4 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 4, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('4');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 5 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 5, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('5');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 6 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 6, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('6');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 7 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 7, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('7');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 8 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 8, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('8');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 9 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 9, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('9');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 10 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 10, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('10');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 11 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 11, function(err, respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('11');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 12 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 12, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('12');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 13 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 13, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('13');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 14 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 14, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('14');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 15 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 15, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('15');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 16 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 16, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('16');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 17 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 17, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('17');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 18 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 18, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('18');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 19 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 19, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  7000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('19');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+	it("Test 20 Expect response to return array of 100,000 records, test id to match response testId and client id to match response clientId ", function(){
+
+		var ret;
+		runs(function() {
+			caller.getService2(1, 20, function(err,respose){
+				ret = respose;
+			});
+        });
+        waitsFor(function() {
+            return ret;
+        },  8000);
+
+		runs(function(){
+             expect(ret.clientId).toBe('1');
+             console.log('test id: ' + ret.testId);
+             expect(ret.testId).toBe('20');
+             expect(ret.records.length).toBe(100000);
+        });
+	});
+
+});
