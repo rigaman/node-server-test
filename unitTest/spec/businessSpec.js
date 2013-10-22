69ec6d5aef1ed22f400363c774ebaf1334274547
Philip <philip@highground.com>
2013-10-22 12:58:17 -0500

    tests

--- unitTest/spec/businessSpec.js
+++ unitTest/spec/businessSpec.js
@@ -1,5 +1,5 @@
 var business = require('../../lib/business/test1.js'),
-	business2 = require('../../lib/business/test2.js'),
+	businessTest = require('../../lib/business/test2.js'),
 	events = require('events'),
 	eventEmitter = new events.EventEmitter();
 
@@ -12,18 +12,12 @@ describe("Business unit tests", function(){
 		});
 	});
 	it("Expect business module to retrun proper json", function(){
-		// var result;
-		// business2.getJson({clientId: 1, testId: 1});
-		// eventEmitter.on('getJsonComplete', function(data){
-		// 	console.log(data);
-		// 	result = data;
-		// });
-		// waitsFor(function(){ return result;});
-		// runs(function(){
-  // 			expect(data.clientId).toBe(1);
-		// 	expect(data.testId).toBe(1);
-		// 	expect(data.count).toBe(1);
-		// });
-
+		var business = new businessTest();
+		business.on('getJsonComplete', function(data){
+			expect(data.clientId).toBe(1);
+			expect(data.testId).toBe(1);
+			expect(data.count).toBe(1);
+		});
+		business.getJson({clientId: 1, testId: 1});
 	});
 });
--- unitTest/spec/serviceSpec.js
+++ unitTest/spec/serviceSpec.js
@@ -1,12 +1,24 @@
 var injector = require('../injector.js'),
 	service1 = injector('./lib/services/testService1.js', {
-	'../business/test1.js': './lib/business/test1.js',
-   	'../repo/test1.js' : './lib/mocks/test1.js',
-   	'../validation/test.js': './lib/validation/test.js'
-});
+		'../business/test1.js': './lib/business/test1.js',
+	   	'../repo/test1.js' : './lib/mocks/test1.js',
+	   	'../validation/test.js': './lib/validation/test.js'
+	}),
+	service2 = injector('./lib/services/testService2.js', {
+		'../validation/validationSpec.js': './lib/validation/validationSpec.js',
+		'../business/test2.js': './lib/business/test2.js',
+	   	'../repo/test2.js' : './lib/mocks/test2.js'
+	});
 describe("Service mock injection test", function(){
 	it("Expect records length to be 1 instead of 1000000", function(){
 		service1.getJson({clientId: 1, testId: 1}, function(err, data){
+			expect(data.clientId).toBe(1);
+			expect(data.testId).toBe(1);
+			expect(data.records.length).toBe(1);
+		});
+	});
+	it("Expect records length to be 1 instead of 1000000", function(){
+		service2.getJson({clientId: 1, testId: 1}, function(err, data){
 			expect(data.clientId).toBe(1);
 			expect(data.testId).toBe(1);
 			expect(data.records.length).toBe(1);
